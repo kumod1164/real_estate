adbffae0797c8b31ffba3267b7049d60e987019f
Teja <saitejanomula143@gmail.com>
2025-05-10 22:52:20 +0530

    pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
 
 const App = () => {
   return (
     <>
-      <h1 className='text-red-500'>hello</h1>
+    
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
