f3deab68e4c413e66a94f27242b335eb0f99fc3b
Teja <saitejanomula143@gmail.com>
2025-05-18 21:17:56 +0530

    Updatelisting fuctionaly create

--- backend/controller/listing.controller.js
+++ backend/controller/listing.controller.js
@@ -48,3 +48,31 @@ export const createListing = async (req, res, next) => {
       next(error);
     }
   };
+
+
+  export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+
+    if (req.user.id !== listing.userRef.toString()) {
+      return next(errorHandler(401, "You can only update your own listing"));
+    }
+    
+    try {
+     const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id ,
+      req.body,
+      {new: true} 
+     );
+     res.status(200).json({
+      success: true,
+      updatedListing
+     })
+    } catch (error) {
+      console.error('Error updating listing:', error);
+      next(error);
+    }
+  };  
--- backend/routes/listing.routes.js
+++ backend/routes/listing.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createListing, deleteListing,  } from '../controller/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../controller/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
+router.post('/create', verifyToken, createListing);
 
-router.post('/create', verifyToken ,createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);   
 
-router.delete('/delete/:id', verifyToken ,deleteListing);   
+router.put('/update/:id', verifyToken, updateListing);
+
 
 export default router;
