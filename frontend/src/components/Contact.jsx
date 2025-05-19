4d3bdfbcd7cb8a3ba1d8a53e74b6bfba2ba5f8ed
Teja <saitejanomula143@gmail.com>
2025-05-19 16:45:56 +0530

    name was getting in contact page

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Contact = () => {
+const Contact = ({listing}) => {
+    const [landlord, setLandlord] = useState(null);
+    const token = localStorage.getItem('token');
+    
+    useEffect(() => {
+        const fetchLandlord = async () => {
+            try {
+                const res = await fetch(`http://localhost:3000/user/${listing.userRef}`,{
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'include'
+                });
+                const data = await res.json();
+                setLandlord(data);
+            } catch (error) {
+                console.error('Error fetching landlord:', error);
+            }
+        };
+        fetchLandlord();
+    }, [listing.userRef]);
+    
   return (
-    <h1>Hello contact</h1>
+    <>
+    {landlord && (
+        <div className=''>
+            <p>Contact <span>{landlord.username}</span></p>
+        </div>
+    )}
+    </>
   )
 }
 
