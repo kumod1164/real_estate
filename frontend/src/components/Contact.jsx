5f5227bae790eefea3a8ee731de677cee4a54ab5
Teja <saitejanomula143@gmail.com>
2025-05-20 16:51:54 +0530

    mailto was updated

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-    
+
+
 const Contact = ({listing}) => {
     const [landlord, setLandlord] = useState(null);
     const [message, setMessage] = useState(''); 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { Navigation} from 'swiper/modules';
+import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
 
 const Home = () => {
@@ -55,7 +53,7 @@ const Home = () => {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Bhavani Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
